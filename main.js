4dbbe04eeec97605695fdf08f750d8fc097d1c26
Diana Jurgielewicz <dmjurg@gmail.com>
2016-07-05 16:44:38 -0400

    included imitation fonts that closely(ish) mirror widths of mock up fonts and adjusted measurements

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+$(document).ready(function() {
+  //Stack menu when collapsed
+  $('#navbar-collapse-target').on('show.bs.collapse', function() {
+      $('.nav-pills').addClass('nav-stacked');
+  });
 
+  //Unstack menu when not collapsed
+  $('#navbar-collapse-target').on('hide.bs.collapse', function() {
+      $('.nav-pills').removeClass('nav-stacked');
+  });
+});
