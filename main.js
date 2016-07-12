fb942df72d3fc26e7586d309af91880684e96f88
Diana Jurgielewicz <dmjurg@gmail.com>
2016-07-12 11:15:51 -0400

    added scrolling

--- main.js
+++ main.js
@@ -8,4 +8,21 @@ $(document).ready(function() {
   $('#navbar-collapse-target').on('hide.bs.collapse', function() {
       $('.nav-pills').removeClass('nav-stacked');
   });
+
+  //Smooth scrolling 
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
 });
