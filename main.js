eb803d5478a6e7969bd3739d61dc22e5388fcd57
Diana Jurgielewicz <dmjurg@gmail.com>
2016-07-12 17:54:05 -0400

    js prevent default issue on panels fixed

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
   });
 
   //Smooth scrolling 
-    $('a[href*="#"]:not([href="#"])').click(function() {
+    $('.nav a[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
